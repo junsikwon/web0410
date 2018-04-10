b8e6c740380de77fe32ab4b8cf0a6884383d64a7
junsikwon <37567362+junsikwon@users.noreply.github.com>
2018-04-10 14:45:41 +0900

    우엉
    우어어어

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 var express = require('express');
 var app = express();
+var _ = require('underscore');
+ var bodyParser = require('body-parser');
 
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json()); // support json encoded bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+ app.use(bodyParser.json()); // support json encoded bodies
+ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', (req, res) => {
   res.charset = 'UTF-8';
+  console.log(req.query);
   res.send('GET으로 넘어온 name은 '+ req.query.name + '입니다.');
 } );
+app.get('/rowcol', (req, res) => {
+  res.charset = 'UTF-8';
+  var checkbox = '<input type ="checkbox">';
+  var col = req.query.col;
+  var row = req.query.col;
 
-/*
+  var string = '';
+  string +='<form>'
+  for(var i =0;i<col;i++){
+  string += checkbox;
+}
+
+  for(var i =0;i<row;i++){
+  result += string;
+}
+
+  res.send(result);
+} );
+//tempLate
+app.get('/template', (req, res) => {
+  res.charset = 'UTF-8';
+  var tp1 = _.template(
+    '<h1>안녕하세요? 제 이름은 <%=name%>이고 나이는 <%=age%>에요.</h1>'
+  )
+  var obj = {name :"홍길동",age:300};
+  res.send(tp1(obj));
+} );
 app.post('/', (req, res) => {
   res.charset = 'UTF-8';
   res.send('POST로 넘어온 name은 '+ req.body.name + '입니다.');
 } );
-*/
+
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
